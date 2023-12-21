2eb8d692221078fe57ff4239db676a1f67102b96
Cesar Laurentin <claurentin@gmail.com>
2023-12-20 22:19:10 -0600

    cleaning

--- src/main/scala/com/parkinglot/Solution.scala
+++ src/main/scala/com/parkinglot/Solution.scala
@@ -7,7 +7,7 @@ object Solution {
   def main(args: Array[String]): Unit = {
     val records = readFile("visit_data_iso8601.csv")
     val results = foregoneRatioBySite(records)
-    results.sortBy(_._2)(Ordering[Double].reverse).foreach{ r =>
+    results.sortBy(_._2).reverse.foreach{ r =>
       println(s"Site Id: ${r._1}, Forgone ratio: ${BigDecimal(r._2).setScale(2, BigDecimal.RoundingMode.HALF_UP)}")
     }
   }
